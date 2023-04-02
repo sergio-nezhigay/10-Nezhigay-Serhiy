976912fae7acd227f1ca84471411d10c4b78d86c
Sergio Nezhigay <abc1971h@gmail.com>
2023-04-02 10:25:31 +0300

    add output

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const resultEl = document.querySelector(".result");
+let task = "<h1> HomeWork #10 </h1>\n";
+
 function main(a = 2, b = 3, c) {
   if (typeof c === "function") {
     return c(sum(a, b));
@@ -12,4 +15,9 @@ function cb1(x) {
   return 2 * x;
 }
 
-console.log(main(10, 20, cb1));
+const result = main(10, 20, cb1);
+console.log(result);
+
+task += result;
+
+resultEl.insertAdjacentHTML("beforeend", task);
